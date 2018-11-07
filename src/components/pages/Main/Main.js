44ef351a415a44529bfba656ac3035094fef1913
Will Miller <willmiller@Wills-MacBook-Pro.local>
2018-11-07 11:28:06 -0500

    finished css for gallery images

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -3,6 +3,10 @@ import './Main.css';
 // component imports here.
 import NavBar from '../../utils/Navbar/navbar';
 import Header from '../../utils/Header/header';
+import Container from '../../utils/grid/container/Container';
+import Row from '../../utils/grid/row/Row';
+import Col from '../../utils/grid/col/Col';
+// import Card from '../../utils/card/card';
 
 
 class Portfolio extends Component {
@@ -35,10 +39,28 @@ class Portfolio extends Component {
                 {/* this is where the next whitespace will go */}
                 <div className="section white">
                     <div className="row container">
-                        <h2 className="aboutHeader"> About Me! </h2>
-                         <p className="grey-text text-darken-3 lighten-3"> heres where all of the about field text will go. there are three paragraphs so ill need three p tags.</p>
-                         <p className="grey-text text-darken-3 lightne-3"> Here is the second paragraph of the about section.</p>
-                         <p className="grey-text text-darken-3 lightne-3"> Here is the third paragraph of the about section.</p>
+                        <h2 className="aboutHeader"> Gallery </h2>
+                        <Container>
+                            <Row>
+                                <Col size='md-6'>
+                                    {/* <Card src={require('../../../images/300x300PlaceHolder.png')} /> */}
+                                    <img className='parallaxImages' src={require('../../../images/ZigRigResized.png')} alt='placeholder' />
+                                    <img className='parallaxImages' src={require('../../../images/300x300PlaceHolder.png')} alt='placeholder' />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col size='md-6'>
+                                    <img className='parallaxImages' src={require('../../../images/300x300PlaceHolder.png')} alt='placeholder' />
+                                    <img className='parallaxImages' src={require('../../../images/300x300PlaceHolder.png')} alt='placeholder' />
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col size='md-6'>
+                                    <img className='parallaxImages' src={require('../../../images/zeldaClickyGameResized.png')} alt='placeholder' />
+                                    <img className='parallaxImages' src={require('../../../images/manchesterHangManResized.png')} alt='placeholder' />
+                                </Col>
+                            </Row>
+                        </Container>
                     </div>
                 </div>
                 {/* extra room for readability */}
@@ -50,7 +72,7 @@ class Portfolio extends Component {
                 {/* this is where the third and final whitespace will go */}
                 <div className="section white">
                     <div className="row container">
-                        <h2 className="aboutHeader"> About Me! </h2>
+                        <h2 className="aboutHeader"> Technologies </h2>
                          <p className="grey-text text-darken-3 lighten-3"> heres where all of the about field text will go. there are three paragraphs so ill need three p tags.</p>
                          <p className="grey-text text-darken-3 lightne-3"> Here is the second paragraph of the about section.</p>
                          <p className="grey-text text-darken-3 lightne-3"> Here is the third paragraph of the about section.</p>
